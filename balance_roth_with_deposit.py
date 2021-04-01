5e8659ea4a32e9db2ca6ad157d4a122a9c117804
Matt Youngberg <matt_youngberg@outlook.com>
2021-04-01 09:49:30 -0600

    Removed argument that was implied by other required arguments. Made the BalancesCalculator operate with Decimals instead of floats, which is better practice for money applications.

--- balance_roth_with_deposit.py
+++ balance_roth_with_deposit.py
@@ -10,7 +10,6 @@ class Args:
 
     def __init__(self):
         self.parser = self.create_arg_parser()
-        self.account_balance: Optional[float] = None
         self.deposit_amount: Optional[float] = None
         self.fzrox_amount: Optional[float] = None
         self.fxnax_amount: Optional[float] = None
@@ -28,8 +27,6 @@ class Args:
         )
 
     def add_args_to_parser(self) -> None:
-        self.parser.add_argument('account_balance', type=float,
-                                 help='The total balance of the roth account')
         self.parser.add_argument('deposit_amount', type=float,
                                  help='The amount that has recently been ' +
                                       'deposited')
@@ -46,45 +43,31 @@ class Args:
 class BalancesCalculator:
 
     def __init__(self, args: Args):
-        self._account_balance: float = args.account_balance
-        self._fzrox_balance: float = args.fzrox_amount
-        self._fxnax_balance: float = args.fxnax_amount
-        self.check_sum_of_balances()
-        self._deposit_amount: float = args.deposit_amount
+        self._account_balance: Decimal = Decimal(str(args.fzrox_amount)) \
+                                       + Decimal(str(args.fxnax_amount))
+        self._fzrox_balance: Decimal = Decimal(str(args.fzrox_amount))
+        self._fxnax_balance: Decimal = Decimal(str(args.fxnax_amount))
+        self._deposit_amount: Decimal = Decimal(str(args.deposit_amount))
         self._balance_after_deposit = self._account_balance + \
                                       self._deposit_amount
         self.deposit_insufficient_to_balance = False
-        self._fzrox_percentage: float = STOCK_PERCENTAGE / 100
-        self._fxnax_percentage: float = 1 - self._fzrox_percentage
+        self._fzrox_percentage: Decimal = Decimal(str(STOCK_PERCENTAGE / 100))
+        self._fxnax_percentage: Decimal = 1 - self._fzrox_percentage
         self._target_fzrox_amount = self._balance_after_deposit * \
                                     self._fzrox_percentage
         self._target_fxnax_amount = self._balance_after_deposit * \
                                     self._fxnax_percentage
-        self._to_fzrox: float = self.get_fzrox_amount()
-        self._to_fxnax: float = self.get_fxnax_amount()
+        self._to_fzrox: Decimal = self.get_fzrox_amount()
+        self._to_fxnax: Decimal = self.get_fxnax_amount()
 
-    def check_sum_of_balances(self):
-        if Decimal(str(self._fzrox_balance)) + Decimal(
-                str(self._fxnax_balance)) != Decimal(
-                str(self._account_balance)):
-            raise ValueError(
-                'The FZROX and FXNAX balances must sum to the account balance:'
-                f' {self.get_fzrox_balance_string()} + '
-                f'{self.get_fxnax_balance_string()} != '
-                f'{self.get_account_balance_string()}')
-
-    def get_fzrox_amount(self) -> float:
-        target: float = (self._account_balance + self._deposit_amount) * \
-                        self._fzrox_percentage
-        diff: float = target - self._fzrox_balance
+    def get_fzrox_amount(self) -> Decimal:
+        diff: Decimal = self._target_fzrox_amount - self._fzrox_balance
         if self._deposit_amount < diff:
             self.deposit_insufficient_to_balance = True
         return diff
 
-    def get_fxnax_amount(self) -> float:
-        target: float = (self._account_balance + self._deposit_amount) * \
-                        self._fxnax_percentage
-        diff: float = target - self._fxnax_balance
+    def get_fxnax_amount(self) -> Decimal:
+        diff: Decimal = self._target_fxnax_amount - self._fxnax_balance
         if self._deposit_amount < diff:
             self.deposit_insufficient_to_balance = True
         return diff
